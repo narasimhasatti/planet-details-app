2ae20209071ba4dd62d73bf758015b6c828a17a3
Narasimha satti <umanarasimha.satti1@t-mobile.com>
2020-06-06 20:02:00 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatTabsModule } from '@angular/material/tabs';
 import { AppComponent } from './app.component';
+import { PlanetListComponent } from './planet-list/planet-list.component';
+import { FavouriteListComponent } from './planet-list/favourite-list/favourite-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetListComponent,
+    FavouriteListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
